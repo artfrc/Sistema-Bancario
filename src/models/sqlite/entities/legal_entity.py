0b0bf2eb839d6cc5973d796b14723a521ef31f11
Arthur <crfernandesarthur@gmail.com>
2024-11-17 17:44:58 -0300

    feat: add restrictions in daatabase (not null, unique)

--- src/models/sqlite/entities/legal_entity.py
+++ src/models/sqlite/entities/legal_entity.py
@@ -1,17 +1,17 @@
 from sqlalchemy import Column, String, BIGINT
 from src.models.sqlite.settings.base import Base
 
-class LegalEntity(Base):
+class LegalEntityTable(Base):
    __tablename__ = 'legal_entity'
    
    id = Column(BIGINT, primary_key=True)
-   billing = Column(float)
-   age = Column(int)
-   trade_name = Column(String)
-   phone_number = Column(String)
-   corpoate_email = Column(String)
+   billing = Column(float, nullable=False)
+   age = Column(int, nullable=False)
+   trade_name = Column(String, nullable=False)
+   phone_number = Column(String, nullable=False, unique=True)
+   corpoate_email = Column(String, nullable=False, unique=True)
    category = Column(String)
-   balance = Column(float)
+   balance = Column(float, nullable=False)
    
    def __repr__(self):
       return f'Legal Entity: Trade Name: {self.trade_name}, phone number: {self.phone_number}, email: {self.corpoate_email}'
--- src/models/sqlite/entities/natural_person.py
+++ src/models/sqlite/entities/natural_person.py
@@ -1,17 +1,17 @@
 from sqlalchemy import Column, String, BIGINT
 from src.models.sqlite.settings.base import Base
 
-class NaturalPerson(Base):
+class NaturalPersonTable(Base):
    __tablename__ = 'natural_person'
    
    id = Column(BIGINT, primary_key=True)
-   monthly_income = Column(float)
-   age = Column(int)
-   name = Column(String)
-   phone_number = Column(String)
-   email = Column(String)
+   monthly_income = Column(float, nullable=False)
+   age = Column(int, nullable=False)
+   name = Column(String, nullable=False)
+   phone_number = Column(String, nullable=False, unique=True)
+   email = Column(String,nullable=False, unique=True)
    category = Column(String)
-   balance = Column(float)
+   balance = Column(float, nullable=False)
    
    def __repr__(self):
       return f'Natural Person: name: {self.name}, phone number: {self.phone_number}, email: {self.email}'
